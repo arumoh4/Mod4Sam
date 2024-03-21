bd0d5a68e88681ccd201a87d82b3748dba99df56
Ekaterina <miits1186@gmail.com>
2024-03-21 15:09:39 +0400

    finish project

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import myphoto4 from '$lib/images/myphoto4.jpeg';
+</script>
+
+
+<div class="container">
+<img alt="Моя кошка" src={myphoto4} width="400" />
+
+<h1>Про мою кошку</h1>
+<p>Немного про мою кошку:
+    <br>1. Её зовут Мурка
+    <br>2. Имя ей придумала бабушка
+    <br>3. Она любит есть
+    <br>Об остальном я расскажу в другой раз.
+</p>
+<h1> Где можно найти Мурку</h1>
+<p>На диване</p>
+<p>На подоконнике</p>
+<p>На моей кровати</p>
+</div>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+</style>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import myphoto1 from "$lib/images/myphoto1.jpeg";
+  import myphoto2 from "$lib/images/myphoto2.jpeg";
+  import myphoto3 from "$lib/images/myphoto3.jpeg";
+  import myphoto4 from "$lib/images/myphoto4.jpeg";
+  import myphoto5 from "$lib/images/myphoto5.jpeg";
+  import myphoto6 from "$lib/images/myphoto6.jpeg";
+</script>
+
+<div class="container">
+  <img alt="Моя кошка 1" src={myphoto1} width="400" />
+  <br />
+  <img alt="Моя кошка 2" src={myphoto2} width="400" />
+  <br />
+  <img alt="Моя кошка 3" src={myphoto3} width="400" />
+  <br />
+  <img alt="Моя кошка 4" src={myphoto4} width="400" />
+  <br />
+  <img alt="Моя кошка 5" src={myphoto5} width="400" />
+  <br />
+  <img alt="Моя кошка 6" src={myphoto6} width="400" />
+</div>
+
+<style>
+  .container {
+    margin: 50px auto;
+    max-width: 800px;
+    padding: 0 20px;
+  }
+</style>
